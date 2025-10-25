84f568af9517a372b925ab0cf1637370e6ab0952
Xdoxgg <m.zhuk007@gmail.com>
2025-10-25 16:49:17 +0300

    start add simple services

---
+++ Services/todoService.js
@@ -0,0 +1,18 @@
+async function getTodoByUserId(id) {
+    let response = await fetch("https://jsonplaceholder.typicode.com/todos ");
+    const data = await response.json();
+    let result = [];
+    data.forEach(e => {
+        if (e.userId == id) {
+            result.push(e.email)
+        }
+    })
+    return result;
+}
+
+
+async function getAllTodos() {
+    let response = await fetch("https://jsonplaceholder.typicode.com/todos ");
+    return await response.json();
+}
+
