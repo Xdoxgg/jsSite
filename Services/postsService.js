b792efcf7a048c057786f9fe1102f3e121dfee3a
Xdoxgg <m.zhuk007@gmail.com>
2025-10-25 17:24:19 +0300

    add services

---
+++ Services/postsService.js
@@ -0,0 +1,18 @@
+
+async function getAllPostsWithComments() {
+    let response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    let posts = await response.json();
+    response = await fetch("https://jsonplaceholder.typicode.com/comments");
+    const comments = await response.json();
+    posts.forEach(post => {
+        post.comments = []
+        comments.forEach(comment => {
+            if(comment.postId == post.id) {}
+            post.comments.push(comment);
+            // console.log(comment)
+        })
+    })
+    return posts;
+}
+
+
